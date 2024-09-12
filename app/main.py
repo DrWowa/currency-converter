8992a39c62ff0b4a6e05f48f7a430081aa3584bc
Vladimir Mosienko <vm.drwowa@gmail.com>
2024-09-12 14:08:42 +0400

    Add converter

--- app/main.py
+++ app/main.py
@@ -6,3 +6,16 @@ app = FastAPI()
 @app.get("/ping")
 def pong():
     return {"ping": "pong!"}
+
+
+def convert(from_symbol, to_symbol, amount, rates):
+    rate = (rates.get(to_symbol) or {}).get(from_symbol, None)
+
+    if rate is None:
+        rate = (rates.get(from_symbol) or {}).get(to_symbol, None)
+        rate = 1 / rate if rate else None
+
+    if rate is None:
+        return None
+
+    return amount * rate
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,32 @@
 from http import HTTPStatus
 
+import pytest
+
+from app.main import convert
+
 
 def test_ping(client):
     response = client.get("/ping")
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {"ping": "pong!"}
+
+
+_convert_options = (
+    # "from_symbol", "to_symbol", "amount", "rates", "expected"
+    ("USD", "JPY", 2, {"JPY": {"USD": 150}}, 300),
+    ("JPY", "EUR", 1_000, {"EUR": {"JPY": 0.05}}, 50),
+    # inversed rate
+    ("USD", "JPY", 2, {"USD": {"JPY": 1 / 150}}, 300),
+    ("JPY", "EUR", 1_000, {"JPY": {"EUR": 1 / 0.05}}, 50),
+    # no rate
+    ("USD", "JPY", 2, {"USD": None}, None),
+    ("USD", "JPY", 2, {}, None),
+)
+
+
+@pytest.mark.parametrize(
+    ("from_symbol", "to_symbol", "amount", "rates", "expected"),
+    _convert_options,
+)
+def test_convert(from_symbol, to_symbol, amount, rates, expected):
+    assert convert(from_symbol, to_symbol, amount, rates) == expected
